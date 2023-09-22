667e6279499425e3756eba9c0f690a75ebe5bc8d
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-09-22 11:26:03 +0330

    add routing and madules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
 
+
+const routes: Routes = [
+  { path: 'day', loadChildren: () => import('./day-view/day-view.module').then(m => m.DayViewRoutingModule) },
+  { path: 'week', loadChildren: () => import('./week-view/week-view.module').then(m => m.WeekViewRoutingModule) },
+  { path: 'month', loadChildren: () => import('./month-view/month-view.module').then(m => m.MonthViewRoutingModule) },
+  { path: 'year', loadChildren: () => import('./year-view/year-view.module').then(m => m.YearViewRoutingModule) },
+  { path: '', redirectTo: '/month', pathMatch: 'full' }, // Redirect to the default view
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { HeaderComponent } from './header/header.component';
+
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatInputModule } from '@angular/material/input';
+import { MatNativeDateModule } from '@angular/material/core';
+import { FormsModule } from '@angular/forms';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+
+import { MonthViewCalenderComponent } from './month-view-calender/month-view-calender.component';
+import { DayViewCalenderComponent } from './day-view-calender/day-view-calender.component';
+import { WeekViewCalenderComponent } from './week-view-calender/week-view-calender.component';
+import { YearViewCalenderComponent } from './year-view-calender/year-view-calender.component';
+import { AppointmentFormComponent } from './appointment-form/appointment-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    HeaderComponent,
+    MonthViewCalenderComponent,
+    DayViewCalenderComponent,
+    WeekViewCalenderComponent,
+    YearViewCalenderComponent,
+    AppointmentFormComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    BrowserAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    FormsModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
